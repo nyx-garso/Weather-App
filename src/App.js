92d907c32abd19193651760c1460f5429f5d0690
GOreiro <tinygenesis22@gmail.com>
2025-03-08 21:57:47 +0800

    Fix API Issues

--- src/App.js
+++ src/App.js
@@ -60,7 +60,7 @@ const App = () => {
 
     try {
       const response = await fetch(
-        `http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
+        `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5&appid=${API_KEY}`
       );
       const data = await response.json();
 
